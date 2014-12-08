5a0117fb731c9f827d6460cf015996c9b4479619
Dmitry Matveev <dmitriy.matveev@corp.mail.ru>
2014-12-08 19:34:01 +0300

    Fix for nameless global variables

--- src/na/okutane/api/cfg/GlobalVariableCache.java
+++ src/na/okutane/api/cfg/GlobalVariableCache.java
@@ -1,14 +1,30 @@
 package na.okutane.api.cfg;
 
+import na.okutane.cpp.ParserListener;
+import na.okutane.cpp.llvm.SWIGTYPE_p_LLVMOpaqueModule;
 import org.springframework.stereotype.Component;
 
 /**
  * @author <a href="mailto:dmitriy.g.matveev@gmail.com">Dmitriy Matveev</a>
  */
 @Component
-public class GlobalVariableCache {
+public class GlobalVariableCache implements ParserListener {
+    int count;
+
     public RValue get(String name, Type type) {
+        if (name.isEmpty()) {
+            name = "global" + count++;
+        }
         return new GlobalVar(name, type);
+    }
+
+    @Override
+    public void onModuleStarted(SWIGTYPE_p_LLVMOpaqueModule module) {
+        count = 0;
+    }
+
+    @Override
+    public void onModuleFinished(SWIGTYPE_p_LLVMOpaqueModule module) {
     }
 
     public static class GlobalVar extends TypedValue {
