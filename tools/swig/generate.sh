a4c3700e1b238c4d2c27bc5532017d37356ab568
Dmitry Matveev <dmitriy.g.matveev@gmail.com>
2016-09-08 01:11:35 +0300

    swig

--- tools/swig/generate.sh
+++ tools/swig/generate.sh
@@ -46,9 +46,9 @@ LLVM_LIBS=
 
 DEBUG=-g
 
-echo "swig $LLVM_INCLUDE $STD_INCLUDES -java -outdir $OUTDIR -package na.okutane.cpp.llvm -v -debug-tmsearch -debug-tmused bitreader.i > swigout.txt"
+echo "swig $LLVM_INCLUDE -java -outdir $OUTDIR -package na.okutane.cpp.llvm -v -debug-tmsearch -debug-tmused bitreader.i"
 
-swig $LLVM_INCLUDE $STD_INCLUDES -java -outdir $OUTDIR -package na.okutane.cpp.llvm -v -debug-tmsearch -debug-tmused bitreader.i > swigout.txt
+swig $LLVM_INCLUDE -java -outdir $OUTDIR -package na.okutane.cpp.llvm -v -debug-tmsearch -debug-tmused bitreader.i
 swig -E $LLVM_INCLUDE $STD_INCLUDES -java bitreader.i > swigprep.txt
 gcc -c bitreader_wrap.c -D__STDC_CONSTANT_MACROS -D__STDC_LIMIT_MACROS $JAVA_INCLUDES $LLVM_INCLUDE $STD_INCLUDES $DEBUG
 g++ -c helpers.cpp -D__STDC_CONSTANT_MACROS -D__STDC_LIMIT_MACROS $JAVA_INCLUDES $LLVM_INCLUDE $DEBUG
