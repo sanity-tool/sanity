240f9d8df500fb1e192a3db9505f61ba0f2da3a5
Dmitry Matveev <dmitriy.g.matveev@gmail.com>
2016-09-08 03:36:50 +0300

    swig fixed, compilation errors again

--- tools/swig/generate.sh
+++ tools/swig/generate.sh
@@ -2,22 +2,40 @@
 # Exit on failure
 set -e
 
-llvm-config --includedir
+LLVM_CONFIG=llvm-config
+$LLVM_CONFIG --version >/dev/null 2>&1 || LLVM_CONFIG=/usr/local/opt/llvm/bin/llvm-config
 
-llvm-config --components
-llvm-config --libdir
-llvm-config --cxxflags
-llvm-config --ldflags
+$LLVM_CONFIG --version
 
-sudo apt-get -qq update
-sudo apt-get install -y swig
+$LLVM_CONFIG --includedir
+
+$LLVM_CONFIG --components
+$LLVM_CONFIG --libdir
+$LLVM_CONFIG --cxxflags
+$LLVM_CONFIG --ldflags
+
+#sudo apt-get -qq update
+swig -version >/dev/null 2>&1 || sudo apt-get install -y swig
+
+swig -version
 
 OUTDIR="../../src/main/java/na/okutane/cpp/llvm"
 
-rm $OUTDIR/*.java || echo already removed
+rm -rf $OUTDIR || echo already removed
 
-JAVA_INCLUDES="-I$JAVA_HOME/include/ -I$JAVA_HOME/include/linux/"
-#JAVA_INCLUDES="-I$JAVA_HOME/include/ -I$JAVA_HOME/include/darwin/"
+
+case `uname` in
+    Darwin)
+        JAVA_INCLUDES="-I$JAVA_HOME/include/ -I$JAVA_HOME/include/darwin/"
+    ;;
+    Linux)
+        JAVA_INCLUDES="-I$JAVA_HOME/include/ -I$JAVA_HOME/include/linux/"
+    ;;
+    *)
+        echo Unknown environment: `uname`
+        exit 1
+    ;;
+esac
 
 STD_INCLUDES=
 
@@ -32,8 +50,9 @@ DLL_NAME=irreader.so
 
 #apt-get install llvm
 
+
 #use release build of llvm
-LLVM_INCLUDE="-I`llvm-config --includedir`"
+LLVM_INCLUDE="-I`$LLVM_CONFIG --includedir`"
 LLVM_LIBS=
 
 #use clang
@@ -48,12 +67,10 @@ DEBUG=-g
 
 echo "swig $LLVM_INCLUDE -java -outdir $OUTDIR -package na.okutane.cpp.llvm -v -debug-tmsearch -debug-tmused bitreader.i"
 
-swig $LLVM_INCLUDE -java -outdir java -package na.okutane.cpp.llvm -v -debug-tmsearch -debug-tmused bitreader.i
+mkdir $OUTDIR
+swig $LLVM_INCLUDE -java -outdir $OUTDIR -package na.okutane.cpp.llvm -v bitreader.i
 swig -E $LLVM_INCLUDE $STD_INCLUDES -java bitreader.i > swigprep.txt
 
 gcc -c bitreader_wrap.c -D__STDC_CONSTANT_MACROS -D__STDC_LIMIT_MACROS $JAVA_INCLUDES $LLVM_INCLUDE $STD_INCLUDES $DEBUG
 g++ -c helpers.cpp -D__STDC_CONSTANT_MACROS -D__STDC_LIMIT_MACROS $JAVA_INCLUDES $LLVM_INCLUDE $DEBUG -fPIC
 gcc -shared bitreader_wrap.o helpers.o $LLVM_LIBS -o $DLL_NAME
-
-#rm *.o
-#rm *.cxx
