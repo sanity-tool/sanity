5728eefd512a813330382e6bb5dd62d668595321
Dmitry Matveev <dmitriy.g.matveev@gmail.com>
2016-09-08 04:26:08 +0300

    fpic

--- tools/swig/generate.sh
+++ tools/swig/generate.sh
@@ -60,7 +60,6 @@ swig -E $LLVM_INCLUDE $STD_INCLUDES -java bitreader.i > swigprep.txt
 
 gcc -c bitreader_wrap.c -D__STDC_CONSTANT_MACROS -D__STDC_LIMIT_MACROS $JAVA_INCLUDES $LLVM_INCLUDE $STD_INCLUDES $DEBUG -fPIC
 
-echo g++ -c helpers.cpp -D__STDC_CONSTANT_MACROS -D__STDC_LIMIT_MACROS $JAVA_INCLUDES $LLVM_INCLUDE $DEBUG -fPIC
-g++ -c helpers.cpp -D__STDC_CONSTANT_MACROS -D__STDC_LIMIT_MACROS $JAVA_INCLUDES $CPPFLAGS
+g++ -c helpers.cpp -D__STDC_CONSTANT_MACROS -D__STDC_LIMIT_MACROS $JAVA_INCLUDES $CPPFLAGS -fPIC
 
 gcc -stdlib=libc++ -shared bitreader_wrap.o helpers.o -o $DLL_NAME $LDFLAGS
