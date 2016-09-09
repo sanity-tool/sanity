32155f5ee31819709e5d4d73c0e5301b31ceef6c
Dmitry Matveev <dmitriy.g.matveev@gmail.com>
2016-09-09 21:28:44 +0300

    -brew llvm

--- tools/swig/generate.sh
+++ tools/swig/generate.sh
@@ -1,9 +1,26 @@
 #!/bin/sh
-
 # Exit on failure
 set -e
 
-# platform specific configuration
+LLVM_CONFIG=llvm-config
+$LLVM_CONFIG --version >/dev/null 2>&1 || LLVM_CONFIG=/usr/local/opt/llvm/bin/llvm-config
+
+$LLVM_CONFIG --version
+
+$LLVM_CONFIG --includedir
+
+$LLVM_CONFIG --components
+$LLVM_CONFIG --libdir
+$LLVM_CONFIG --cxxflags
+$LLVM_CONFIG --ldflags
+
+#sudo apt-get -qq update
+swig -version >/dev/null 2>&1 || sudo apt-get install -y swig
+
+swig -version
+
+
+
 case `uname` in
     Darwin)
         LLVM_REMOTE="http://llvm.org/releases/3.8.0/clang+llvm-3.8.0-x86_64-apple-darwin.tar.xz"
@@ -43,14 +60,13 @@ tar xf $LLVM_LOCAL -C $DEPENDENCIES_DIR
 
 # find
 LLVM_CONFIG=`find $TARGET_DIR -name llvm-config`
-CC=`find $TARGET_DIR -name clang`
-CXX=`find $TARGET_DIR -name clang++`
 
-# check tools availability
-$LLVM_CONFIG --version
-$CC --version
-$CXX --version
-swig -version
+
+#find / 2>/dev/null|grep libc\\.
+
+#find / 2>/dev/null|grep libc++\\.
+
+#find / 2>/dev/null|grep libstdc
 
 CPPFLAGS=`$LLVM_CONFIG --cppflags`
 LDFLAGS=`$LLVM_CONFIG --ldflags`
@@ -76,20 +92,19 @@ rm -rf $CPP_OUT || echo already removed
 mkdir -p $JAVA_OUT
 mkdir -p $CPP_OUT
 swig $LLVM_INCLUDE -java -outdir $JAVA_OUT -package na.okutane.cpp.llvm -o $CPP_OUT/bitreader_wrap.c -v bitreader.i
-swig -E $LLVM_INCLUDE $STD_INCLUDES -java bitreader.i > swigprep.txt
 
 OBJ_DIR="../../target/native/static"
 SOBJ_DIR="../../target/native/shared"
 mkdir -p $OBJ_DIR
 mkdir -p $SOBJ_DIR
 
-$CC -c $CPP_OUT/bitreader_wrap.c -D__STDC_CONSTANT_MACROS -D__STDC_LIMIT_MACROS $JAVA_INCLUDES $LLVM_INCLUDE $STD_INCLUDES $DEBUG -fPIC -o $OBJ_DIR/wrappers.o
+clang -c $CPP_OUT/bitreader_wrap.c -D__STDC_CONSTANT_MACROS -D__STDC_LIMIT_MACROS $JAVA_INCLUDES $LLVM_INCLUDE $STD_INCLUDES $DEBUG -fPIC -o $OBJ_DIR/wrappers.o
 
-COMPILE_HELPERS="$CXX -c helpers.cpp -D__STDC_CONSTANT_MACROS -D__STDC_LIMIT_MACROS $CPPFLAGS -fPIC -std=c++11 -o $OBJ_DIR/helpers.o"
+COMPILE_HELPERS="clang++ -c helpers.cpp -D__STDC_CONSTANT_MACROS -D__STDC_LIMIT_MACROS $CPPFLAGS -fPIC -std=c++11 -o $OBJ_DIR/helpers.o"
 echo $COMPILE_HELPERS
 eval $COMPILE_HELPERS
 
-LINK_CMD="$CXX -shared $STD_LIBS $LIBS $OBJ_DIR/wrappers.o $OBJ_DIR/helpers.o -o $SOBJ_DIR/$DLL_NAME -L/usr/local/opt/libffi/lib -L/usr/local/opt/llvm/lib -Wl,-rpath,/usr/local/opt/llvm/lib $LDFLAGS"
+LINK_CMD="clang++ -shared $STD_LIBS $LIBS $OBJ_DIR/wrappers.o $OBJ_DIR/helpers.o -o $SOBJ_DIR/$DLL_NAME $LDFLAGS"
 echo $LINK_CMD
 eval $LINK_CMD
 
