4d2873496f0cf01db306d312e4564a317c4b8c60
Dmitry Matveev <dmitriy.g.matveev@gmail.com>
2016-09-16 01:34:22 +0300

    install clang for testing

--- install.sh
+++ install.sh
@@ -6,10 +6,13 @@ set -e
 if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
     apt-get install llvm # main dependency
     apt-get install swig # wrappers generator
+
+    apt-get install clang
 fi
 
 if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
     brew update
+
     brew install llvm # main dependency
     brew install crosstool-ng # to replace undesired function from llvm core
     brew install swig # wrappers generator
