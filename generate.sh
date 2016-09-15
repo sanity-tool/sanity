9ad4095bb6d09b8321babb77b92a4d397d6f374b
Dmitry Matveev <dmitriy.g.matveev@gmail.com>
2016-09-16 01:26:56 +0300

    Linux setup

--- generate.sh
+++ generate.sh
@@ -7,16 +7,25 @@ LLVM_CONFIG=llvm-config
 $LLVM_CONFIG --version >/dev/null 2>&1 || LLVM_CONFIG=/usr/local/opt/llvm/bin/llvm-config
 
 case `uname` in
-    Darwin)
-        JAVA_INCLUDES="-I$JAVA_HOME/include/ -I$JAVA_HOME/include/darwin/"
-
-        HACK_DLL_NAME=libdebughack.jnilib
-        DLL_NAME=libirreader.jnilib
-    ;;
     Linux)
+        CC=gcc
+        CXX=g++
+        LD=g++
+        OBJCOPY=objcopy
+
         JAVA_INCLUDES="-I$JAVA_HOME/include/ -I$JAVA_HOME/include/linux/"
 
         DLL_NAME=libirreader.so
+    ;;
+    Darwin)
+        CC=clang
+        CXX=clang++
+        LD=clang++
+        OBJCOPY=gobjcopy
+
+        JAVA_INCLUDES="-I$JAVA_HOME/include/ -I$JAVA_HOME/include/darwin/"
+
+        DLL_NAME=libirreader.jnilib
     ;;
     *)
         echo Unknown environment: `uname`
@@ -51,20 +60,20 @@ mkdir -p $SOBJ_DIR
 
 swig $LLVM_INCLUDE -java -outdir $JAVA_OUT -package na.okutane.cpp.llvm -o $CPP_OUT/bitreader_wrap.c -v $SRC_DIR/bitreader.i
 
-clang -c $CPP_OUT/bitreader_wrap.c -D__STDC_CONSTANT_MACROS -D__STDC_LIMIT_MACROS $JAVA_INCLUDES $LLVM_INCLUDE -I/usr/local/opt/llvm/include $DEBUG -fPIC -o $OBJ_DIR/wrappers.o
+$CC -c $CPP_OUT/bitreader_wrap.c -D__STDC_CONSTANT_MACROS -D__STDC_LIMIT_MACROS $JAVA_INCLUDES $LLVM_INCLUDE -I/usr/local/opt/llvm/include $DEBUG -fPIC -o $OBJ_DIR/wrappers.o
 
-clang++ -c $SRC_DIR/helpers.cpp -D__STDC_CONSTANT_MACROS -D__STDC_LIMIT_MACROS $CPPFLAGS -I/usr/local/opt/llvm/include $DEBUG -fPIC -std=c++11 -o $OBJ_DIR/helpers.o
+$CXX -c $SRC_DIR/helpers.cpp -D__STDC_CONSTANT_MACROS -D__STDC_LIMIT_MACROS $CPPFLAGS -I/usr/local/opt/llvm/include $DEBUG -fPIC -std=c++11 -o $OBJ_DIR/helpers.o
 
 if [ -z "$REAL_LLVM" ]; then
     # llvm drops debug information (hence source refs) from some compilers, code below disabled that logic.
 
     # compile replacement code.
-    clang++ -c $SRC_DIR/debughack.cpp -D__STDC_CONSTANT_MACROS -D__STDC_LIMIT_MACROS $CPPFLAGS -I/usr/local/opt/llvm/include $DEBUG -fPIC -std=c++11 -o $OBJ_DIR/debughack.o
+    $CXX -c $SRC_DIR/debughack.cpp -D__STDC_CONSTANT_MACROS -D__STDC_LIMIT_MACROS $CPPFLAGS -I/usr/local/opt/llvm/include $DEBUG -fPIC -std=c++11 -o $OBJ_DIR/debughack.o
 
     # remove logic from llvm library.
     cp /usr/local/Cellar/llvm/3.8.1/lib/libLLVMCore.a $OBJ_DIR/libLLVMCore.a
     chmod +w $OBJ_DIR/libLLVMCore.a
-    gobjcopy -v --target mach-o-x86-64 --strip-symbol __ZN4llvm16UpgradeDebugInfoERNS_6ModuleE $OBJ_DIR/libLLVMCore.a
+    $OBJCOPY -v --target mach-o-x86-64 --strip-symbol __ZN4llvm16UpgradeDebugInfoERNS_6ModuleE $OBJ_DIR/libLLVMCore.a
 
     # link against "hacked" library and replacement code.
     LIBS="${LIBS/-lLLVMCore/} $OBJ_DIR/libLLVMCore.a $OBJ_DIR/debughack.o"
@@ -72,4 +81,4 @@ if [ -z "$REAL_LLVM" ]; then
     LDFLAGS="-Wl,-allow_sub_type_mismatches ${LDFLAGS}"
 fi
 
-clang++ -shared $LIBS $OBJ_DIR/wrappers.o $OBJ_DIR/helpers.o -o $SOBJ_DIR/$DLL_NAME -L/usr/local/opt/libffi/lib $LDFLAGS $DEBUG
+$CXX -shared $LIBS $OBJ_DIR/wrappers.o $OBJ_DIR/helpers.o -o $SOBJ_DIR/$DLL_NAME -L/usr/local/opt/libffi/lib $LDFLAGS $DEBUG
---
+++ install.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+# Exit on failure
+set -e
+
+if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
+    apt-get install llvm # main dependency
+    apt-get install swig # wrappers generator
+fi
+
+if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
+    brew update
+    brew install llvm # main dependency
+    brew install crosstool-ng # to replace undesired function from llvm core
+    brew install swig # wrappers generator
+fi
