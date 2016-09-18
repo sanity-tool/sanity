edf4f740d2bc11cfdc6ff0b2435e505fcb8a3f85
Dmitry Matveev <dmitriy.g.matveev@gmail.com>
2016-09-18 03:55:51 +0300

    update java in install.sh

--- install.sh
+++ install.sh
@@ -4,6 +4,8 @@
 #set -e
 
 if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
+    sudo apt-get update && sudo apt-get install oracle-java8-installer
+
     sudo add-apt-repository ppa:ubuntu-toolchain-r/test -y
     sudo apt-get update
     sudo apt-get install gcc-4.9 g++-4.9
