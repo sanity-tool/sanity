035d20040fdc2e4014e4b152d8a705dab8594d11
Dmitry Matveev <dmitriy.g.matveev@gmail.com>
2016-09-20 03:14:43 +0300

    cleanup

--- install.sh
+++ install.sh
@@ -9,7 +9,4 @@ if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
     brew install llvm # main dependency
     brew install crosstool-ng # to replace undesired function from llvm core
     brew install swig # wrappers generator
-
-    TEST_SUPPORTED_EXTS=".c;.cpp;.m"
-    TEST_SUPPORTED_DIRS="c;cpp;o-c"
 fi
