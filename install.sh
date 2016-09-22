af57d4200d78808b1c769a225ab3732abbe36039
Dmitry Matveev <dmitriy.g.matveev@gmail.com>
2016-09-22 23:02:10 +0300

    installing java

--- install.sh
+++ install.sh
@@ -6,9 +6,10 @@ set -e
 if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
     brew update
 
+    brew cast install java
     brew install llvm # main dependency
     brew install crosstool-ng # to replace undesired function from llvm core
     brew install swig # wrappers generator
 
-    brew install llvm33
+    brew install llvm33 # explicitly testing older llvm to ensure older xcode support
 fi
