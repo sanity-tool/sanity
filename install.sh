79ea21265704e0b956919d0158edc49c0b66b3c4
Dmitry Matveev <dmitriy.g.matveev@gmail.com>
2016-10-04 01:56:00 +0300

    try inline install

--- install.sh
+++ install.sh
@@ -1,15 +1,3 @@
 #!/bin/bash
 
-# Exit on failure
-set -e
 
-if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
-    brew update
-
-    brew cask install java
-    brew install llvm # main dependency
-    brew install crosstool-ng # to replace undesired function from llvm core
-    brew install swig # wrappers generator
-
-    brew install llvm33 # explicitly testing older llvm to ensure older xcode support
-fi
