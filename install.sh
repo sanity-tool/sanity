5712b1bc5a021a105c47380ed2d723d972690ed5
Dmitry Matveev <dmitriy.g.matveev@gmail.com>
2016-10-04 21:55:55 +0300

    try more llvms

--- install.sh
+++ install.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
 
+# Exit on failure
+set -e
 
+if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
+    brew update
+
+    brew cask install java
+    brew install llvm # main dependency
+    brew install crosstool-ng # to replace undesired function from llvm core
+    brew install swig # wrappers generator
+
+    [ -z $EXTRA_BREW ] || brew install $EXTRA_BREW
+fi
