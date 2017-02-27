0ac4ccfcd5532a3b0a858944d27ed65eee8fb320
Dmitry Matveev <dmitriy.g.matveev@gmail.com>
2017-02-27 19:43:15 +0300

    realpath

--- generate.sh
+++ generate.sh
@@ -7,7 +7,7 @@ set -e
 case `uname` in
     Linux)
         if [[ ! -f "cmake-3.4.3-Linux-x86_64/bin/cmake" ]]; then wget --no-check-certificate http://cmake.org/files/v3.4/cmake-3.4.3-Linux-x86_64.tar.gz && tar -xvf cmake-3.4.3-Linux-x86_64.tar.gz; fi
-        CMAKE=cmake-3.4.3-Linux-x86_64/bin/cmake
+        CMAKE=`realpath cmake-3.4.3-Linux-x86_64/bin/cmake`
         CC=gcc-4.9
         CXX=g++-4.9
         LD=g++-4.9
