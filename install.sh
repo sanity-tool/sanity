2547f0fa2e0a12490c98da6f5e7eb5b5bc319eaa
Dmitry Matveev <dmitriy.g.matveev@gmail.com>
2017-02-28 02:47:41 +0300

    more fixes

--- install.sh
+++ install.sh
@@ -7,8 +7,7 @@ if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
     brew update
 
     brew cask install java
-    # brew install llvm # main dependency
-    brew install cmake # to build custom llvm
+    brew install llvm # main dependency
     brew install swig # wrappers generator
 
     [ -z $EXTRA_BREW ] || brew install $EXTRA_BREW
