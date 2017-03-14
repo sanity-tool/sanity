95f31994a99ee490374b54c45b724ae90b152676
Dmitry Matveev <dmitriy.g.matveev@gmail.com>
2017-03-14 05:59:54 +0300

    local ok

--- parser-native/src/main/java/ru/urururu/sanity/cpp/NativeBytecodeParser.java
+++ parser-native/src/main/java/ru/urururu/sanity/cpp/NativeBytecodeParser.java
@@ -1,23 +1,18 @@
 package ru.urururu.sanity.cpp;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import ru.urururu.sanity.CfgUtils;
+import ru.urururu.sanity.api.BytecodeParser;
 import ru.urururu.sanity.api.Cfg;
 import ru.urururu.sanity.api.cfg.*;
 import ru.urururu.sanity.cpp.llvm.SWIGTYPE_p_LLVMOpaqueBasicBlock;
 import ru.urururu.sanity.cpp.llvm.SWIGTYPE_p_LLVMOpaqueModule;
 import ru.urururu.sanity.cpp.llvm.SWIGTYPE_p_LLVMOpaqueValue;
 import ru.urururu.sanity.cpp.llvm.bitreader;
-import ru.urururu.sanity.utils.TempFileWrapper;
 
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
+import java.io.File;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
@@ -25,11 +20,7 @@ import java.util.List;
  * @author <a href="mailto:dmitriy.g.matveev@gmail.com">Dmitry Matveev</a>
  */
 @Component
-public class ParserImpl extends Parser {
-    private static final Logger LOGGER = LoggerFactory.getLogger(Parser.class);
-
-    @Autowired
-    ClangParametersFactory parametersFactory;
+public class NativeBytecodeParser implements BytecodeParser {
     @Autowired
     CfgUtils cfgUtils;
     @Autowired
@@ -42,93 +33,6 @@ public class ParserImpl extends Parser {
     ParserListener[] listeners;
     @Autowired
     ConstCache constants;
-
-    public List<Cfg> parse(String filename) throws ParseException {
-        LOGGER.info("filename = {}", filename);
-        try {
-            try (TempFileWrapper objFile = new TempFileWrapper("result", ".bc")) {
-                try (TempFileWrapper errFile = new TempFileWrapper("result", ".err")) {
-                    String[] parameters = parametersFactory.getParameters(filename, objFile.getAbsolutePath());
-                    LOGGER.info("parameters = {}", Arrays.toString(parameters));
-
-                    ProcessBuilder pb = new ProcessBuilder(parameters);
-
-                    pb.inheritIO();
-                    pb.redirectError(ProcessBuilder.Redirect.to(errFile.getFile()));
-
-                    Process process = pb.start();
-
-                    int resultCode = process.waitFor();
-
-                    if (resultCode == 0) {
-                        SWIGTYPE_p_LLVMOpaqueModule m = bitreader.parse(objFile.getAbsolutePath());
-
-                        if (m == null) {
-                            return Collections.emptyList();
-                        }
-
-                        //bitreader.LLVMDumpModule(m); // todo move to separate listener for test/debug
-                        for (ParserListener listener : listeners) {
-                            listener.onModuleStarted(m);
-                        }
-
-                        try {
-                            ArrayList<Cfg> result = new ArrayList<>();
-
-                            SWIGTYPE_p_LLVMOpaqueValue function = bitreader.LLVMGetFirstFunction(m);
-                            while (function != null) {
-                                try {
-                                    if (bitreader.LLVMGetFirstBasicBlock(function) != null) {
-                                        CfgBuildingCtx ctx = new CfgBuildingCtx(typeParser, function);
-
-                                        SWIGTYPE_p_LLVMOpaqueBasicBlock entryBlock = bitreader.LLVMGetEntryBasicBlock(function);
-
-                                        Cfe entry = processBlock(ctx, entryBlock);
-
-                                        SWIGTYPE_p_LLVMOpaqueBasicBlock block = bitreader.LLVMGetFirstBasicBlock(function);
-                                        block = bitreader.LLVMGetNextBasicBlock(block);
-                                        while (block != null) {
-                                            Cfe blockEntry = processBlock(ctx, block);
-                                            Cfe label = ctx.getLabel(bitreader.LLVMBasicBlockAsValue(block));
-
-                                            label.setNext(blockEntry);
-
-                                            block = bitreader.LLVMGetNextBasicBlock(block);
-                                        }
-
-                                        entry = cfgUtils.removeNoOps(entry);
-
-                                        result.add(new Cfg(bitreader.LLVMGetValueName(function), entry));
-                                    }
-                                } catch (Exception e) {
-                                    System.err.println("Can't parse function: " + bitreader.LLVMGetValueName(function));
-                                    e.printStackTrace(System.err);
-                                }
-                                function = bitreader.LLVMGetNextFunction(function);
-                            }
-
-                            Cfe entry = parseGlobalInitializers(m);
-                            if (entry != null) {
-                                result.add(new Cfg("<module init>", entry));
-                            }
-
-                            return result;
-                        } finally {
-                            for (ParserListener listener : listeners) {
-                                listener.onModuleFinished(m);
-                            }
-                            bitreader.LLVMDisposeModule(m);
-                        }
-                    } else {
-                        String error = new String(Files.readAllBytes(Paths.get(errFile.getAbsolutePath())));
-                        throw new ParseException(resultCode, error);
-                    }
-                }
-            }
-        } catch (InterruptedException | IOException e) {
-            throw new ParseException(e);
-        }
-    }
 
     private Cfe parseGlobalInitializers(SWIGTYPE_p_LLVMOpaqueModule module) {
         Cfe first = null;
@@ -208,5 +112,68 @@ public class ParserImpl extends Parser {
         }
 
         return first;
+    }
+
+    @Override
+    public List<Cfg> parse(File file) {
+        SWIGTYPE_p_LLVMOpaqueModule m = bitreader.parse(file.getAbsolutePath());
+
+        if (m == null) {
+            return Collections.emptyList();
+        }
+
+        //bitreader.LLVMDumpModule(m); // todo move to separate listener for test/debug
+        for (ParserListener listener : listeners) {
+            listener.onModuleStarted(m);
+        }
+
+        try {
+            ArrayList<Cfg> result = new ArrayList<>();
+
+            SWIGTYPE_p_LLVMOpaqueValue function = bitreader.LLVMGetFirstFunction(m);
+            while (function != null) {
+                try {
+                    if (bitreader.LLVMGetFirstBasicBlock(function) != null) {
+                        CfgBuildingCtx ctx = new CfgBuildingCtx(typeParser, function);
+
+                        SWIGTYPE_p_LLVMOpaqueBasicBlock entryBlock = bitreader.LLVMGetEntryBasicBlock(function);
+
+                        Cfe entry = processBlock(ctx, entryBlock);
+
+                        SWIGTYPE_p_LLVMOpaqueBasicBlock block = bitreader.LLVMGetFirstBasicBlock(function);
+                        block = bitreader.LLVMGetNextBasicBlock(block);
+                        while (block != null) {
+                            Cfe blockEntry = processBlock(ctx, block);
+                            Cfe label = ctx.getLabel(bitreader.LLVMBasicBlockAsValue(block));
+
+                            label.setNext(blockEntry);
+
+                            block = bitreader.LLVMGetNextBasicBlock(block);
+                        }
+
+                        entry = cfgUtils.removeNoOps(entry);
+
+                        result.add(new Cfg(bitreader.LLVMGetValueName(function), entry));
+                    }
+                } catch (Exception e) {
+                    System.err.println("Can't parse function: " + bitreader.LLVMGetValueName(function));
+                    e.printStackTrace(System.err);
+                }
+
+                function = bitreader.LLVMGetNextFunction(function);
+            }
+
+            Cfe entry = parseGlobalInitializers(m);
+            if (entry != null) {
+                result.add(new Cfg("<module init>", entry));
+            }
+
+            return result;
+        } finally {
+            for (ParserListener listener : listeners) {
+                listener.onModuleFinished(m);
+            }
+            bitreader.LLVMDisposeModule(m);
+        }
     }
 }
