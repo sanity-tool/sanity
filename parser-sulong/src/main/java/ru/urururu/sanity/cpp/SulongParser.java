b89f502eb34a6c23e711c8edd5f1d00555e5f7cb
Dmitry Matveev <dmitriy.g.matveev@gmail.com>
2017-04-04 18:56:43 +0700

    sulong: use reuse

--- parser-sulong/src/main/java/ru/urururu/sanity/cpp/SulongParser.java
+++ parser-sulong/src/main/java/ru/urururu/sanity/cpp/SulongParser.java
@@ -1,15 +1,21 @@
 package ru.urururu.sanity.cpp;
 
+import com.oracle.truffle.api.source.MissingNameException;
 import com.oracle.truffle.api.source.Source;
 import com.oracle.truffle.llvm.parser.BitcodeParserResult;
+import com.oracle.truffle.llvm.parser.model.ModelModule;
+import com.oracle.truffle.llvm.parser.model.blocks.InstructionBlock;
+import com.oracle.truffle.llvm.parser.model.functions.FunctionDefinition;
+import com.oracle.truffle.llvm.parser.model.globals.GlobalVariable;
+import com.oracle.truffle.llvm.parser.model.symbols.instructions.Instruction;
+import com.oracle.truffle.llvm.parser.model.visitors.ModelVisitor;
 import com.oracle.truffle.llvm.runtime.LLVMLanguage;
+import com.oracle.truffle.llvm.runtime.types.symbols.Symbol;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-import ru.urururu.sanity.api.BytecodeParser;
-import ru.urururu.sanity.api.Cfg;
-import ru.urururu.sanity.api.cfg.Cfe;
+import ru.urururu.sanity.api.AbstractBytecodeParser;
+import ru.urururu.sanity.api.cfg.LValue;
 
-import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
@@ -18,29 +24,80 @@ import java.util.List;
  * @author <a href="mailto:dmitriy.g.matveev@gmail.com">Dmitry Matveev</a>
  */
 @Component
-public class SulongParser implements BytecodeParser {
+public class SulongParser extends AbstractBytecodeParser<ModelModule, com.oracle.truffle.llvm.runtime.types.Type,
+        Symbol, Instruction, InstructionBlock, SuCfgBuildingCtx> {
     @Autowired
     SulongParsersFacade parsers;
 
     @Override
-    public List<Cfg> parse(File file) {
-        List<Cfg> result = new ArrayList<>();
+    protected Iterable<? extends Symbol> getGlobals(ModelModule module) {
+        List<Symbol> result = new ArrayList<>();
 
-        try {
-            Source source = Source.newBuilder(file).mimeType(LLVMLanguage.LLVM_BITCODE_MIME_TYPE).build();
-            BitcodeParserResult bitcodeParserResult = BitcodeParserResult.getFromSource(source);
-
-            CfgBuilder moduleParser = new CfgBuilder(parsers, result);
-            bitcodeParserResult.getModel().accept(moduleParser);
-
-            Cfe entry = moduleParser.getInitFirst();
-            if (entry != null) {
-                result.add(new Cfg("<module init>", entry));
+        module.accept(new ModelVisitor() {
+            @Override
+            public void visit(GlobalVariable variable) {
+                result.add(variable);
             }
-        } catch (IOException e) {
-            throw new IllegalStateException(e);
-        }
+        });
 
         return result;
+    }
+
+    @Override
+    protected void parseGlobalInitializer(ru.urururu.sanity.api.cfg.CfgBuilder builder, Symbol initializer, LValue globalToInitialize) {
+
+    }
+
+    @Override
+    protected Symbol getInitializer(Symbol global) {
+        return null;
+    }
+
+    @Override
+    protected ModelModule parseModule(String absolute) throws IOException {
+        Source source;
+        try {
+            source = Source.newBuilder(absolute).mimeType(LLVMLanguage.LLVM_BITCODE_MIME_TYPE).build();
+        } catch (MissingNameException e) {
+            throw new IllegalStateException(e);
+        }
+        BitcodeParserResult bitcodeParserResult = BitcodeParserResult.getFromSource(source);
+
+        return bitcodeParserResult.getModel();
+    }
+
+    @Override
+    protected Iterable<? extends Symbol> getFunctions(ModelModule module) {
+        List<Symbol> result = new ArrayList<>();
+
+        module.accept(new ModelVisitor() {
+            @Override
+            public void visit(FunctionDefinition function) {
+                result.add(function);
+            }
+        });
+
+        return result;
+    }
+
+    @Override
+    protected Iterable<InstructionBlock> getBlocks(Symbol function) {
+        FunctionDefinition functionDefinition = (FunctionDefinition) function;
+        return functionDefinition.getBlocks();
+    }
+
+    @Override
+    protected String toDebugString(Symbol value) {
+        return value.toString();
+    }
+
+    @Override
+    protected Symbol toValue(InstructionBlock block) {
+        return block;
+    }
+
+    @Override
+    protected SuCfgBuildingCtx createCtx(Symbol function) {
+        return new SuCfgBuildingCtx(parsers, (FunctionDefinition) function);
     }
 }
