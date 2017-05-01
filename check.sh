21bb34a8aa4b5af575f1351f5dbe486d87267383
Dmitry Matveev <dmitriy.g.matveev@gmail.com>
2017-05-01 21:29:41 +0300

    try updated travis yml

--- check.sh
+++ check.sh
@@ -3,7 +3,26 @@
 set -e
 
 for LANG in $LANGS; do
-  echo $LANG
+  if [ "$TRAVIS_BRANCH" = "master" ] && [ "$TRAVIS_PULL_REQUEST" = "false" ]; then
+    # run normal analysis
+    mvn -P $SANITY_PROFILE \
+      package sonar:sonar \
+      -Dsonar.host.url=$SONAR_HOST_URL \
+      -Dsonar.login=$SONAR_TOKEN
+  elif [ "$TRAVIS_PULL_REQUEST" != "false" ] && [ -n "${GITHUB_TOKEN-}" ]; then
+    # run PR analysis
+    mvn -P $SANITY_PROFILE \
+      package sonar:sonar \
+      -Dsonar.host.url=$SONAR_HOST_URL \
+      -Dsonar.login=$SONAR_TOKEN \
+      -Dsonar.analysis.mode=preview \
+      -Dsonar.github.oauth=$GITHUB_TOKEN \
+      -Dsonar.github.repository=$TRAVIS_REPO_SLUG \
+      -Dsonar.github.pullRequest=$TRAVIS_PULL_REQUEST
+  else
+    # run tests
+    mvn -P $SANITY_PROFILE verify
+  fi
 
   bash <(curl -s https://codecov.io/bash) -F $LANG
 done
