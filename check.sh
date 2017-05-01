78b5f988509bb40b1a2c8dc92cea2e3c3a76e91b
Dmitry Matveev <dmitriy.g.matveev@gmail.com>
2017-05-01 22:34:59 +0300

    Try foreach in script

--- check.sh
+++ check.sh
@@ -2,27 +2,23 @@
 # Exit on failure
 set -e
 
-for LANG in $LANGS; do
-  if [ "$TRAVIS_BRANCH" = "master" ] && [ "$TRAVIS_PULL_REQUEST" = "false" ]; then
+if [ "$TRAVIS_BRANCH" = "master" ] && [ "$TRAVIS_PULL_REQUEST" = "false" ]; then
     # run normal analysis
     mvn -P $SANITY_PROFILE \
-      package sonar:sonar \
-      -Dsonar.host.url=$SONAR_HOST_URL \
-      -Dsonar.login=$SONAR_TOKEN
-  elif [ "$TRAVIS_PULL_REQUEST" != "false" ] && [ -n "${GITHUB_TOKEN-}" ]; then
+        package sonar:sonar \
+		-Dsonar.host.url=$SONAR_HOST_URL \
+		-Dsonar.login=$SONAR_TOKEN
+elif [ "$TRAVIS_PULL_REQUEST" != "false" ] && [ -n "${GITHUB_TOKEN-}" ]; then
     # run PR analysis
-    mvn -P $SANITY_PROFILE \
-      package sonar:sonar \
-      -Dsonar.host.url=$SONAR_HOST_URL \
-      -Dsonar.login=$SONAR_TOKEN \
-      -Dsonar.analysis.mode=preview \
-      -Dsonar.github.oauth=$GITHUB_TOKEN \
-      -Dsonar.github.repository=$TRAVIS_REPO_SLUG \
-      -Dsonar.github.pullRequest=$TRAVIS_PULL_REQUEST
-  else
+	mvn -P $SANITY_PROFILE \
+	    package sonar:sonar \
+		-Dsonar.host.url=$SONAR_HOST_URL \
+		-Dsonar.login=$SONAR_TOKEN \
+		-Dsonar.analysis.mode=preview \
+		-Dsonar.github.oauth=$GITHUB_TOKEN \
+		-Dsonar.github.repository=$TRAVIS_REPO_SLUG \
+		-Dsonar.github.pullRequest=$TRAVIS_PULL_REQUEST
+else
     # run tests
     mvn -P $SANITY_PROFILE verify
-  fi
-
-  bash <(curl -s https://codecov.io/bash) -F $LANG
-done
+fi
