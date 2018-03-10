5ee87f861cb76393aa0ea2cb5055ffa302bdba1d
Dmitry Matveev <dmitriy.g.matveev@gmail.com>
2018-03-11 01:08:45 +0300

    remote service invokable

--- parser-remote/src/main/java/ru/urururu/sanity/cpp/NativeBytecodeParser.java
+++ parser-remote/src/main/java/ru/urururu/sanity/cpp/NativeBytecodeParser.java
@@ -1,22 +1,18 @@
 package ru.urururu.sanity.cpp;
 
-import okhttp3.OkHttpClient;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import ru.urururu.sanity.CfgUtils;
 import ru.urururu.sanity.api.BytecodeParser;
 import ru.urururu.sanity.api.Cfg;
-import ru.urururu.sanity.api.cfg.*;
-import ru.urururu.sanity.cpp.llvm.*;
-import ru.urururu.util.Iterables;
+import ru.urururu.sanity.api.cfg.ConstCache;
 
 import java.io.*;
 import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
 import java.net.URL;
-import java.net.URLConnection;
 import java.nio.file.Files;
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
@@ -36,6 +32,7 @@ public class NativeBytecodeParser implements BytecodeParser {
             URL url = new URL("http://10.0.75.2:32768/parse");
             HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
             urlConnection.setRequestMethod("POST");
+            urlConnection.addRequestProperty("Content-Type", "application/octet-stream");
             urlConnection.setDoOutput(true);
             OutputStream os = urlConnection.getOutputStream();
             os.write(Files.readAllBytes(file.toPath()));
@@ -46,25 +43,19 @@ public class NativeBytecodeParser implements BytecodeParser {
             System.out.println("POST Response Code :: " + responseCode);
 
             if (responseCode == HttpURLConnection.HTTP_OK) { //success
-                BufferedReader in = new BufferedReader(new InputStreamReader(
-                        urlConnection.getInputStream()));
-                String inputLine;
-                StringBuffer response = new StringBuffer();
+                ObjectMapper mapper = new ObjectMapper();
+                JsonNode module = mapper.readTree(urlConnection.getInputStream());
 
-                while ((inputLine = in.readLine()) != null) {
-                    response.append(inputLine);
-                }
-                in.close();
-
-                // print result
-                System.out.println(response.toString());
+                return parseTree(module);
             } else {
                 throw new IllegalStateException("code was " + responseCode);
             }
         } catch (IOException e) {
             throw new IllegalStateException(e);
         }
+    }
 
-        return Collections.emptyList();
+    private List<Cfg> parseTree(JsonNode module) {
+        return null;
     }
 }
