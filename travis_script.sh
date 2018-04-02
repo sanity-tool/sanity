bb20d390f1fe19b2ae429f3c9569122c25e485b1
Dmitry Matveev <dmitriy.g.matveev@gmail.com>
2018-04-03 02:24:25 +0300

    Update travis_script.sh

--- travis_script.sh
+++ travis_script.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+# Exit on failure
+set -e
+
 for TESTED_LANG in $TESTED_LANGS; do
   mvn -P $SANITY_PROFILE clean test -B
   bash <(curl -s https://codecov.io/bash) -c -F $TESTED_LANG
