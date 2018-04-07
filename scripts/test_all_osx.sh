738048296354c4dffb7fec1cd0b1bc85a601b8f6
Dmitry Matveev <dmitriy.g.matveev@gmail.com>
2018-04-08 01:48:34 +0300

    add llvm 6.0

--- scripts/test_all_osx.sh
+++ scripts/test_all_osx.sh
@@ -3,7 +3,7 @@
 # Exit on failure
 set -e
 
-CLANGS="clang clang-3.3 clang-3.6 clang-3.7 clang-3.8 /usr/local/opt/llvm@4/bin/clang-4.0 /usr/local/opt/llvm@5/bin/clang-5.0"
+CLANGS="clang /usr/local/opt/llvm/bin/clang clang-3.3 clang-3.6 clang-3.7 clang-3.8 /usr/local/opt/llvm@4/bin/clang-4.0 /usr/local/opt/llvm@5/bin/clang-5.0"
 
 for CLANG_BIN in $CLANGS; do
     JAVA_HOME=`/usr/libexec/java_home -v 1.8` mvn clean test -P parser-native
