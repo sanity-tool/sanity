dc71d06371a4bf72ee8968ecd63559b76bc3c7f0
Dmitry Matveev <dmitriy.g.matveev@gmail.com>
2018-04-08 16:30:10 +0300

    try fix newer rust

--- scripts/test_all_osx.sh
+++ scripts/test_all_osx.sh
@@ -3,6 +3,12 @@
 # Exit on failure
 set -e
 
+RUSTS="rustc $HOME/.cargo/bin/rustc"
+
+for RUST_BIN in $RUSTS; do
+    TESTED_LANG=Rust JAVA_HOME=`/usr/libexec/java_home -v 1.8` mvn clean test -P parser-native
+done
+
 CLANGS="clang /usr/local/opt/llvm/bin/clang clang-3.3 clang-3.6 clang-3.7 clang-3.8 /usr/local/opt/llvm@4/bin/clang-4.0 /usr/local/opt/llvm@5/bin/clang-5.0"
 
 for CLANG_BIN in $CLANGS; do
