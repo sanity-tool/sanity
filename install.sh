14831b8915c4b52d4b96ce05a52c5fb6b0eae3e6
Dmitry Matveev <dmitriy.g.matveev@gmail.com>
2018-04-08 06:18:10 +0300

    Restore rust

--- install.sh
+++ install.sh
@@ -15,6 +15,6 @@ if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
     [ -z $EXTRA_BREW ] || brew install $EXTRA_BREW
 fi
 
-if [[ "$TESTED_LANG" == "Rust" ]]; then
+if [[ "$TESTED_LANGS" == "Rust" ]]; then
     curl https://sh.rustup.rs -sSf | sh -s -- -y
 fi
